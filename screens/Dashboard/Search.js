48ff903c712d9b0a9f01e9fee4c764a8ef3ea1ea
Irshad Khan <khanirshad2019@gmail.com>
2023-06-24 09:25:31 +0530

    Search screen Top search and Browse category section

--- screens/Dashboard/Search.js
+++ screens/Dashboard/Search.js
@@ -1,13 +1,144 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
     View,
-    Text
+    Text,
+    Image,
+    TextInput
 } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { Shadow } from 'react-native-shadow-2';
+import {TextButton,CategoryCard} from "../../components"
+import { COLORS,FONTS,SIZES,icons,dummyData } from '../../constants';
+import Animated, { Extrapolate ,interpolate,useAnimatedScrollHandler,useAnimatedStyle,useSharedValue} from 'react-native-reanimated';
+
 
 const Search = () => {
+    const scrollViewRef=useRef()
+
+    function renderTopSearches(){
+        return (
+            <View
+                style={{
+                    marginTop:SIZES.padding
+                }}
+            >
+                <Text
+                    style={{
+                      marginHorizontal: SIZES.padding,
+                      ...FONTS.h2
+                    }}
+                >
+                    Top Searches 
+                </Text>
+
+                <FlatList
+                    horizontal
+                    data={dummyData?.top_searches}
+                    key={'TopSearches'}
+                    keyExtractor={item=>`TopSearches-${item.id}`}
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{
+                        marginTop:SIZES.radius
+                    }}
+                    renderItem={({item,index})=>(
+                        <TextButton 
+                        label={item.label} 
+                        contentContainerStyle={{
+                            paddingVertical:SIZES.radius,
+                            paddingHorizontal:SIZES.padding,
+                            marginLeft:index==0?SIZES.padding:SIZES.radius,
+                            marginRight:index==dummyData?.top_searches.length-1?SIZES.padding:0,
+                            borderRadius:SIZES.radius,
+                            backgroundColor:COLORS.gray10
+                        }} 
+                        labelStyle={{
+                            color:COLORS.gray50,
+                            ...FONTS.h3
+                        }}
+                        
+                        />
+                    )}
+                >
+
+                </FlatList>
+
+            </View>
+        )
+    }
+
+    function renderBrowseCategories(){
+        return (
+            <View
+                style={{
+                    marginTop:SIZES.padding
+                }}
+            >
+                <Text
+                    style={{
+                      marginHorizontal: SIZES.padding,
+                      ...FONTS.h2
+                    }}
+                >
+                    Browse Categories
+                </Text>
+
+                <FlatList
+                    data={dummyData?.categories}
+                    numColumns={2}
+                    scrollEnabled={false}
+                    key={'BrowseCategories'}
+                    keyExtractor={item=>`BrowseCategories-${item.id}`}        
+                    contentContainerStyle={{
+                        marginTop:SIZES.radius
+                    }}
+                    renderItem={({item,index})=>( 
+                        <CategoryCard 
+                            category={item}
+                            containerStyle={{
+                                height:130,
+                                width:(SIZES.width-(SIZES.padding*2)-SIZES.radius)/2,
+                                marginTop:SIZES.radius,
+                                marginLeft:(index)%2==0?SIZES.padding:SIZES.radius
+                                
+                            }}
+                        />
+                    )}
+                >
+
+                </FlatList>
+
+              
+
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>Search</Text>
+        <View
+            style={{
+                flex:1,
+                backgroundColor:COLORS.white
+            }}
+        >
+            <Animated.ScrollView
+                ref={scrollViewRef}
+                contentContainerStyle={{
+                    marginTop:100,
+                    // paddingBottom:300
+                }}
+                showsVerticalScrollIndicator
+                scrollEventThrottle={16}
+                keyboardDismissMode={"on-drag"}
+                // onScroll={}
+                // onScrollEndDrag={}
+            >
+                {/*top searches  */}
+                {renderTopSearches()}
+
+                {/* Browse Category */}
+                {renderBrowseCategories()}
+
+            </Animated.ScrollView>
         </View>
     )
 }
